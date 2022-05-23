c97d76d73fed57fdf10c7860e401db53edd5be3a
Marina Miroslava <argenty@gmail.com>
2022-05-23 16:49:56 +0300

    SPA part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({dish}) {
-        
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        }
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
 
         
     
+
+    /*function RenderComments({comments}) {
+        if (comments != null) 
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return( 
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author},
+                                    {new Intl.DateTimeFormat('en-US', {
+                                        year: 'numeric', 
+                                        month: 'short', 
+                                        day: '2-digit'
+                                    }).format(new Date(Date.Parse(comment.date)))}
+                                    </p>
+                                </li>
+
+                            );
+                        })}
+                    </ul>
+                </div>
+               
+            );
+        else 
+           return(
+               <div></div>
+           );
+    }
+    */
 
     function RenderComments({comments}) {
         if (comments == null) {
@@ -35,7 +67,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
                         year: 'numeric',
                         month: 'short',
                         day: '2-digit'
-                    }).format(new Date(Date.Parse(comment.date)))}
+                    }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </li>
             )
@@ -53,22 +85,32 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
     }
 
     const DishDetail = (props) => {
-
-        if(props.dish == null) {
-            return (<div></div>);
-        }
-
-
-        return (
+        if(props.dish != null) 
+          return (
             <div className="container">
+              <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+              </div>
+            </div>
             <div className="row">
-                <RenderDish dish={ props.dish } />
-                <RenderComments comments={ props.dish.comments } />
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.comments} />
              </div>
              </div>
 
             
+          );
+    else
+        return (
+             <div></div>
         );
+
     }
 
 
