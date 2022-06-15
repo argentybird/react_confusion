39bc71f9b5357260a56459c1005ed33bbc169561
Marina Miroslava <argenty@gmail.com>
2022-06-16 02:24:09 +0300

    Redux Thunk

--- src/redux/dist/configureStore.dev.js
+++ src/redux/dist/configureStore.dev.js
@@ -15,13 +15,19 @@ var _promotions = require("./promotions");
 
 var _leaders = require("./leaders");
 
+var _reduxThunk = _interopRequireDefault(require("redux-thunk"));
+
+var _reduxLogger = _interopRequireDefault(require("redux-logger"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 var ConfigureStore = function ConfigureStore() {
   var store = (0, _redux.createStore)((0, _redux.combineReducers)({
     dishes: _dishes.Dishes,
     comments: _comments.Comments,
     promotions: _promotions.Promotions,
     leaders: _leaders.Leaders
-  }));
+  }), (0, _redux.applyMiddleware)(_reduxThunk["default"], _reduxLogger["default"]));
   return store;
 };
 
